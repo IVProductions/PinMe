0a77c1edf3d637d61b4b7baf40992cf657080173
Simon Hordvik <simon.hordvik@gmail.com>
2013-10-21 17:27:59 -0700

    new user

--- app/app.js
+++ app/app.js
@@ -6,6 +6,10 @@ PinMe.config(function ($routeProvider){
     $routeProvider.when("/", {
         templateUrl:"app/partials/login.html",
         controller:"loginCtrl"
+    })
+    $routeProvider.when("/newuser.html", {
+        templateUrl:"app/partials/newuser.html.html",
+        controller:"newUserCtrl"
     }).otherwise({
         redirectTo:"/"
     })

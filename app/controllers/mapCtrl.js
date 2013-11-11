5f7c7e7c138bff096d7947ccac2ffd2657387d67
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2013-11-11 13:32:25 -0800

    changes

--- app/controllers/mapCtrl.js
+++ app/controllers/mapCtrl.js
@@ -1,41 +1,39 @@
 function mapCtrl($scope, $location){
 
-    var latitude = null;
-    var longitude = null;
 
     $("#map-canvas").css({'height': window.innerHeight});
 
     // Wait for Cordova to load
-    document.addEventListener("deviceready", onDeviceReady, false);
+    //document.addEventListener("deviceready", onDeviceReady, false);
 
     // Cordova is ready
-    function onDeviceReady() {
-        console.log("Device is Ready");
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    }
+    //function onDeviceReady() {
+    //   console.log("Device is Ready");
+    //   navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    //}
 
     // onSuccess Geolocation
-    function onSuccess(position) {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
+    //function onSuccess(position) {
+        //var latitude = position.coords.latitude;
+        //var longitude = position.coords.longitude;
 
-        console.log("lat: "+latitude +" long: "+longitude);
+        //console.log("lat: "+latitude +" long: "+longitude);
 
-        var myOptions = {
-            zoom: 15,
-            center: new google.maps.LatLng(latitude,longitude),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-
-        console.log("new map");
-        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-        console.log("done");
-    }
+     //   var myOptions = {
+    //        zoom: 15,
+    //        center: new google.maps.LatLng(-34.397, 150.644),
+    //        mapTypeId: google.maps.MapTypeId.ROADMAP
+    //    };
+    //
+    //    console.log("new map");
+    //    var map = new google.maps.Map($("#map-canvas"), myOptions);
+    //    console.log("done");
+   // }
 
     // onError Callback receives a PositionError object
-    function onError(error) {
-        alert('code: ' + error.code + '\n message: ' + error.message + '\n');
-    }
+  //  function onError(error) {
+  //      alert('code: ' + error.code + '\n message: ' + error.message + '\n');
+  //  }
 
 /*    // configure marker
     var marker_options = {

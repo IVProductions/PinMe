2a160ba8208448128f6d986a41e875b04bce99c7
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2013-11-19 14:33:07 -0800

    lulz

--- app/services/markerFactory.js
+++ app/services/markerFactory.js
@@ -21,7 +21,10 @@ PinMe.factory("markerFactory", function (stateService, $http) {
 
     $http.post("http://ec2-54-227-8-199.compute-1.amazonaws.com/get_all_markers.php", data).
         success(function(data, status){
-            alert(data);
+            var json = JSON.stringify(eval(data));
+            alert(json[2]);
+
+            //alert(data);
             console.log(data);
         }).
         error(function(data, status){

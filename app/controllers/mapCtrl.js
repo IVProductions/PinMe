b177007fa7fc0fbfe1f80a9fac198f221b36fda6
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2013-11-19 16:31:28 -0800

    asdsad

--- app/controllers/mapCtrl.js
+++ app/controllers/mapCtrl.js
@@ -81,7 +81,7 @@ function mapCtrl($scope, $http, stateService, markerFactory, $location){
             success(function(data, status){
                 console.log("Success!");
                 alert(data);
-                $scope.markers = stateService.functions.getAllMarkers();
+                $scope.markers["temp"] = stateService.functions.getLastMarker();
             }).
             error(function(data, status){
                 console.log("Error");
--- app/services/stateService.js
+++ app/services/stateService.js
@@ -4,6 +4,7 @@ PinMe.factory("stateService", function ($http) {
 
     var currentUser = "";
     var markers = {};
+    var lastMarker = {};
     var functions = {};
 
     functions.getLatitude = function(){
@@ -89,7 +90,7 @@ PinMe.factory("stateService", function ($http) {
                         pincolor = "greypin.png";
                     }
 
-                    markers[''+json.locations[i].id+''] = {
+                    lastMarker["temp"] = {
                         lat: lat,
                         lng: lng,
                         name: markerName,
@@ -104,6 +105,74 @@ PinMe.factory("stateService", function ($http) {
                         })
                     }
                 }
+            }).
+            error(function(data, status){
+                console.log("Error");
+                console.log(data || "No data returned." );
+                console.log(status);
+                alert(data);
+            });
+
+        return lastMarker;
+    };
+
+    functions.getLastMarker = function(){
+
+        var data = {
+            "dummy" : "dummy"
+        };
+
+        $http.post("http://ec2-54-227-8-199.compute-1.amazonaws.com/get_last_marker.php", data).
+            success(function(data, status){
+                var json = JSON.parse(JSON.stringify(eval(data)));
+                //alert(json.locations[0].username);
+
+                var category = json.locations[i].category;
+                var lat = json.locations[i].lat;
+                var lng = json.locations[i].lng;
+                var markerName = json.locations[i].markerName;
+                var description = json.locations[i].description;
+                var imglink = json.locations[i].image_link;
+                var username = json.locations[i].username;
+                var pincolor = "greypin.png";
+
+                if(category == "Recreational"){
+                    pincolor = "redpin.png";
+                }
+                else if (category == "Dining"){
+                    pincolor = "purplepin.png";
+                }
+                else if (category == "Entertainment"){
+                    pincolor = "bluepin.png";
+                }
+                else if (category == "Educational"){
+                    pincolor = "greenpin.png";
+                }
+                else if (category == "Attraction"){
+                    pincolor = "yellowpin.png";
+                }
+                else if (category == "Shops"){
+                    pincolor = "orangepin.png";
+                }
+                else {
+                    pincolor = "greypin.png";
+                }
+
+                markers[''+json.locations[i].id+''] = {
+                    lat: lat,
+                    lng: lng,
+                    name: markerName,
+                    description: description,
+                    category: category,
+                    username: username,
+                    imglink: imglink,
+                    icon: L.icon({
+                        iconUrl: 'Content/img/' + pincolor,
+                        iconSize: [38,55],
+                        iconAnchor: [18,55]
+                    })
+                }
+
             }).
             error(function(data, status){
                 console.log("Error");
